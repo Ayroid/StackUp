e48311aba992f5d4d0d5687bb36ad824507e8d5a
ayroid <ayushsk0000@gmail.com>
2024-07-12 02:49:36 +0530

    feat: frontend basic route setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import { Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Auth, Category, Home } from "./pages";
+
+const DummyNavbar = () => {
+  return (
+    <div className="navbar">
+      <h1>Navbar</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/category">Category</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="mainDiv">
+      <DummyNavbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Auth />} />
+        <Route path="/category/*" element={<Category />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/src/pages/index.jsx
@@ -0,0 +1,3 @@
+export { default as Auth } from "./authPage/authPage";
+export { default as Category } from "./categoryPage/categoryPage";
+export { default as Home } from "./homePage/homePage";
